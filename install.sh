639ced5babdfabe9e6955ad410fa36265ee7da43
Lpokimo <61668752+Lpokimo@users.noreply.github.com>
2022-09-06 20:54:19 -0400

    first commit

--- install.sh
+++ install.sh
@@ -78,9 +78,10 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
 | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
-    | sudo tee /etc/apt/sources.list.d/vscodium.list
+   | sudo tee /etc/apt/sources.list.d/vscodium.list
 
-sudo apt update && sudo apt install codium codium-insiders -y
+    sudo apt update && sudo apt-get install codium 
+
 ###########
 #Vs Codium# End
 ###########
